ef113f0b5ebaf8f623c77aac1c7b2e3331f85db1
Heidi-commits <heidicoding2000@gmail.com>
2024-02-23 16:18:47 +0800

    reservatioin

---
+++ all.js
@@ -0,0 +1,9 @@
+$(document).ready(function() { $('.request-book').click(function(event){
+    $('.box').fadeToggle(500);
+    });
+   });
+   $(document).ready(function() { $('.request-note').click(function(event){
+    $('.note').slideToggle(2000);
+    });
+   });
+   
